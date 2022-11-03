c155cd1376ee13069ff614b964e2c97e00f6d1d5
Chaitya Vora <90527884+vorachaitya@users.noreply.github.com>
2022-11-03 23:24:41 +0530

    ContactUs page added

---
+++ client/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <p>WELCOME</p>
+      <h1>We are the contact MERN Developer</h1>
+    </div>
+  );
+};
+
+export default Contact;
