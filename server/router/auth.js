ee3e531fd280b4092db343c5b7f53b0657b42552
Chaitya Vora <90527884+vorachaitya@users.noreply.github.com>
2022-11-08 22:03:15 +0530

    authentication added

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../model/userSchema");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.get("/", (req, res) => {
   res.send("Hello World auth");
@@ -49,12 +51,14 @@ router.post("/register", async (req, res) => {
 
     if (userExist) {
       return res.status(422).json({ error: "Email already exists" });
+    } else if (password != cpassword) {
+      return res.status(422).json({ error: "Passwords are not matching" });
+    } else {
+      const user = new User({ name, email, phone, work, password, cpassword });
+
+      await user.save();
+      res.status(201).json({ message: "user registered successfully" });
     }
-
-    const user = new User({ name, email, phone, work, password, cpassword });
-
-    await user.save();
-    res.status(201).json({ message: "user registered successfully" });
   } catch (err) {
     console.log(err);
   }
@@ -68,11 +72,25 @@ router.post("/signin", async (req, res) => {
     if (!email || !password) {
       return res.status(400).json({ message: "Please fill the data" });
     }
+
     const userLogin = await User.findOne({ email: email });
+
     if (!userLogin) {
-      res.status(400).json({ error: "user error" });
+      res.status(400).json({ error: "Invalid Credentials" });
     } else {
-      res.json({ message: "user SignIn successful" });
+      const isMatch = await bcrypt.compare(password, userLogin.password);
+      const token = userLogin.generateAuthToken();
+
+      res.cookie("jwttoken", token, {
+        expires: new Date(Date.now() + 25892000000),
+        httpOnly: true,
+      });
+
+      if (!isMatch) {
+        res.status(400).json({ error: "Invalid credentials" });
+      } else {
+        res.json({ message: "user SignIn successful" });
+      }
     }
   } catch (err) {
     console.log(err);
